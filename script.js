0729e6d0be9add4c9a5a0987443125b870284453
EthanDelannoy <ethan.delannoy12@gmail.com>
2024-11-05 22:22:07 +0100

    05-11-24

--- script.js
+++ script.js
@@ -30,26 +30,18 @@ btnprojet.addEventListener( "mouseleave", () =>{
 
 const barre = document.getElementById('barre');
 
-
 function positionBarre(element) {
-    const rect = element.getBoundingClientRect();
-    const barreWidth = rect.width;
-    const barreLeft = rect.left + (rect.width / 2); 
-    barre.style.width = `${barreWidth}px`;
-    barre.style.left = `${barreLeft}px`; 
+    const rect = element.getBoundingClientRect(); 
+    barre.style.width = rect.width + 'px';  
+    barre.style.left = rect.left + rect.width / 2 + 'px';  
 }
 
-
 document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('mouseenter', () => {
-        positionBarre(link);
-        barre.classList.add('active'); 
-    });
+    link.addEventListener('mouseenter', () => positionBarre(link));
 });
 
 document.querySelector('nav').addEventListener('mouseleave', () => {
-    barre.style.width = '0';
-    barre.classList.remove('active'); 
+    barre.style.width = '0'; 
 });
 
 // --------------------------------------------------------------DETAILS PHOTO PROJET-----------------------------------------
